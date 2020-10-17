1df5150837aa2c9859c872fccaa3e164bdcbc752
Chiaoso <67939464+Chiaoso@users.noreply.github.com>
2020-10-17 21:52:58 +0800

    Add files via upload
    they are all empty.

---
+++ Card.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arcane;
+
+/**
+ *
+ * @author chiaoso
+ */
+public class Card {
+    //basic properties of a Card
+    
+    //id of card
+    int cardId;
+    // Name of card
+    String cardName;
+    //Cost of Mana
+    int manaCost;
+    //Level of Spell
+    int spellLevel;
+    //Components of Spell
+    boolean material, somatic, verbal;
+    //Requirement of Circal
+    int[] circal;
+    //Description of Spell
+    String description;
+    
+    
+
+}
---
+++ CardInstance.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arcane;
+
+/**
+ *
+ * @author chiaoso
+ */
+public class CardInstance {
+    //to do...
+    
+    //get the card id and create a card instance 
+    
+    //know how it works
+    
+    //where it is? and show it.
+}
